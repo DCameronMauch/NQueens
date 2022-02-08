ca72ed0815c971c9c2d8e6066b42be0355283d1d
D Cameron Mauch <cam@douglas-cameron.net>
2022-02-08 10:39:10 -0700

    use list pattern matching instead of isEmpty

--- src/main/scala/com/github/dcameronmauch/NQueens.scala
+++ src/main/scala/com/github/dcameronmauch/NQueens.scala
@@ -14,19 +14,17 @@ object NQueens {
     })
 
     @tailrec
-    def recurse(todo: List[State], acc: Set[Set[Int]]): Set[Set[Int]] = {
-      if (todo.isEmpty) acc
-      else {
-        val state: State = todo.head
-        if (state.queens.size == n) recurse(todo.tail, acc + state.queens)
-        else if (state.avail.size < n - state.queens.size) recurse(todo.tail, acc)
+    def recurse(todo: List[State], acc: Set[Set[Int]]): Set[Set[Int]] = todo match {
+      case Nil => acc
+      case state :: tail =>
+        if (state.queens.size == n) recurse(tail, acc + state.queens)
+        else if (state.avail.size < n - state.queens.size) recurse(tail, acc)
         else {
           val next: List[State] = state.avail.toList.map(a =>
             State(state.queens + a, filter(a, state.avail - a))
           )
-          recurse(next ++ todo.tail, acc)
+          recurse(next ++ tail, acc)
         }
-      }
     }
 
     val range: Set[Int] = (0 until n * n).toSet
